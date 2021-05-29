22fa947d1a28a5a8bdd7880d2900c0248c80da4a
Denis <kochanov.denis5@yandex.ru>
2021-05-29 23:27:39 +0300

    Простая сборка gulp для верстки лендингов.

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -2,9 +2,8 @@ const gulp = require('gulp')
 
 const imageMinify = require('./imageMinify')
 const styles = require('./styles')
-const pug2html = require('./pug2html')
 const script = require('./script')
-
+const html = require('./html')
 const server = require('browser-sync').create()
 
 function readyReload(cb) {
@@ -23,7 +22,7 @@ module.exports = function serve(cb) {
     gulp.watch('src/img/*.{png,jpg,svg,webp}', gulp.series(imageMinify, readyReload))
     gulp.watch('src/styles/**/*.scss', gulp.series(styles, cb => gulp.src('build/css').pipe(server.stream()).on('end', cb)))
     gulp.watch('src/js/**/*.js', gulp.series(script, readyReload))
-    gulp.watch('src/pages/**/*.pug', gulp.series(pug2html, readyReload))
+    gulp.watch('src/index.html', gulp.series(html, readyReload))
 
     return cb()
 }
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const { series, parallel } = require('gulp');
 
-const pug2html = require('./gulp/tasks/pug2html')
 const styles = require('./gulp/tasks/styles')
+const html = require('./gulp/tasks/html')
 const script = require('./gulp/tasks/script')
 const fonts = require('./gulp/tasks/fonts')
 const imageMinify = require('./gulp/tasks/imageMinify')
@@ -11,7 +11,7 @@ const serve = require('./gulp/tasks/serve')
 const svgSprite = require('./gulp/tasks/svgSprite')
 */
 
-const dev = parallel(pug2html, styles, script, fonts, imageMinify)
+const dev = parallel(html, styles, script, fonts, imageMinify)
 
 module.exports.start = series(
     clean,
