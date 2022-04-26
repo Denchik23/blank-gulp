7cbb01a2d169031811c7f7d79bb80a13c4bccb20
Denis Kochanov <kochanov.denis5@yandex.ru>
2022-04-26 17:08:54 +0300

    Обновил пакеты и зависимости.

--- gulp-tasks/script.js
+++ gulp-tasks/script.js
@@ -5,18 +5,24 @@ const babel = require('gulp-babel')
 const terser = require('gulp-terser')
 const sourcemaps = require('gulp-sourcemaps')
 const rename = require("gulp-rename")
+const gulpIf = require("gulp-if");
+
+const yargs = require('yargs')
+
+const argv = yargs.argv,
+    production = !!argv.production;
 
 module.exports = function script() {
   return gulp.src('src/js/main.js')
     .pipe(plumber())
     .pipe(eslint())
     .pipe(eslint.format())
-    .pipe(sourcemaps.init())
+    .pipe(gulpIf(!production, sourcemaps.init()))
     .pipe(babel({
         presets: ['@babel/env']
     }))
-    .pipe(terser())
-    .pipe(sourcemaps.write())
+    .pipe(gulpIf(production, terser()))
+    .pipe(gulpIf(!production, sourcemaps.write()))
     .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('build/js'))
 }
